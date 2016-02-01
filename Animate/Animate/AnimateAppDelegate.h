db7f4b1796fae052a3513121d3348a361ec6384d
Srinivas <srinivasattili84@gmail.com>
2016-02-01 10:15:17 +0530

    Animation

---
+++ Animate/Animate/AnimateAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AnimateAppDelegate.h
+//  Animate
+//
+//  Created by Neil Smyth on 9/19/13.
+//  Copyright (c) 2013 Neil Smyth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimateAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
